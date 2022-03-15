253cc7cfaeeafa166d15a28e8d44b0450f4861cf
Sean Murray <sean.j.murray@protonmail.com>
2022-03-15 12:53:52 -0700

    added first route to api to get all docs from database

--- server.py
+++ server.py
@@ -1,11 +1,25 @@
+from flask import jsonify, request
+from flask_pymongo import PyMongo
 import flask
+
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
+mongodb_client = PyMongo(app, uri="mongodb://localhost:27017/checkingTheTime")
+db = mongodb_client.db
 
 
 @app.route('/', methods=['GET'])
 def home():
-    return "<h1>Hello World</h1>"
+    return "I'm just an API"
+
+
+@app.route('/watches/api', methods=['GET'])
+def getapi():
+    data = []
+    for watch in db.watches.find():
+        watch['_id'] = str(watch['_id'])
+        data.append(watch)
+    return jsonify(data)
 
 
 app.run()
